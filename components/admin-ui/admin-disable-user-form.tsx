d22e8812c197b4fd14728dd4305d2ed36f06daf8
mallah-elmehdi <elmehdimallah@gmail.com>
2026-03-18 17:36:27 +0800

    fix missing params

--- components/admin-ui/admin-disable-user-form.tsx
+++ components/admin-ui/admin-disable-user-form.tsx
@@ -6,17 +6,24 @@ import { useForm } from 'react-hook-form';
 import { toast } from 'sonner';
 import P from '../typography/P';
 import { useAdmin } from '@/lib/contexts/admin-context';
+import { useSearchParams } from 'next/navigation';
 
 export default function AdminDisableUserForm({ onSuccess, id, setIsLoading, activate }: AdminDisableUserFormProps) {
     const { setUsersWithPagination } = useAdmin();
     const form = useForm({});
+    // get page from search params
+    const searchParams = useSearchParams();
+    const pageQuery = searchParams.get('page');
+
+    // set page state
+    const page = pageQuery ? Number(pageQuery) : 1;
 
     const onSubmit = async () => {
         setIsLoading(true);
         try {
             await adminDisableUser(id, activate);
             toast.success(`User ${activate ? 'activated' : 'deactivated'} successfully`);
-            const users = await getAdminUsers();
+            const users = await getAdminUsers(page);
             console.log('users', users);
 
             setUsersWithPagination(users);

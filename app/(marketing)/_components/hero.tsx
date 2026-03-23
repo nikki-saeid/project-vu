aaebf87bca492fa0f01a9460664f7c6dcf0d2a99
mallah-elmehdi <elmehdimallah@gmail.com>
2026-03-23 09:21:54 +0800

    update the landing page

--- app/(marketing)/_components/hero.tsx
+++ app/(marketing)/_components/hero.tsx
@@ -5,22 +5,21 @@ import P from '@/components/typography/P';
 import { Button } from '@/components/ui/button';
 import { DEMO_URL } from '@/lib/constants/urls';
 import { IconAppWindow, IconArrowRight } from '@tabler/icons-react';
-import Image from 'next/image';
 
 export default function Hero() {
     return (
-        <section className="mx-auto max-w-5xl grid md:grid-cols-4 items-center grid-cols-1 md:gap-6 gap-4">
-            <div className="flex flex-col col-span-2">
-                <H1 className="max-w-3xl text-balance leading-9 text-4xl sm:text-5xl md:text-6xl">Map it. Share it. Vu it.</H1>
+        <section className="mx-auto max-w-5xl">
+            <div className="flex flex-col">
+                <H1 className="max-w-3xl text-center leading-9 text-3xl sm:text-4xl md:text-5xl">Map it. Share it. Vu it.</H1>
 
-                <P className="text-balance max-w-3xl mt-4 text-xl text-muted-foreground leading-7">
+                <P className="text-balance text-center max-w-3xl mt-4 text-md text-muted-foreground leading-7">
                     Showcase your projects with photos, maps, and a professional profile.
                 </P>
 
-                <div className="mt-8 flex gap-2 sm:flex-row flex-col">
+                <div className="mt-8 flex gap-2">
                     <Link href="/signup" className="flex-1">
                         <Button size="lg" className="w-full">
-                            Create my map
+                            Get started now
                             <IconArrowRight className="size-4" />
                         </Button>
                     </Link>
@@ -31,10 +30,6 @@ export default function Hero() {
                         </Button>
                     </Link>
                 </div>
-            </div>
-
-            <div className="col-span-2">
-                <Image src="/home/hero.webp" alt="hero image an illustration of a map and project" width={1000} height={500} />
             </div>
         </section>
     );
--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,19 +1,17 @@
-import Cta from './_components/cta';
-import Hero from './_components/hero';
-import Features from './_components/features';
-import LiveMap from './_components/live-map';
 import AboutUs from './_components/about-us';
+import Cta from './_components/cta';
+import Features from './_components/features';
+import Hero from './_components/hero';
+import LiveMap from './_components/live-map';
 
 export default function Home() {
- 
-
     return (
         <div className="flex flex-col gap-20 my-20">
             <Hero />
             <Features />
             <LiveMap />
             <AboutUs />
-            {/* <Cta /> */}
+            <Cta />
         </div>
     );
 }
